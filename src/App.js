6718c0e38381dff68f9353a2c7cccbbc377b7bd4
Gaurav Soni <gaurav5678.soni@gmail.com>
2023-03-21 22:14:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+import Weather from "./Components/Weather";
+
+const App = (props) => {
+  const [city, setCity] = useState("");
+  const [currentLocation, setCurrentLocation] = useState("");
+  const [weather, setWeather] = useState("");
+  const [show, setShow] = useState(false);
+  const [icon, setIcon] = useState("");
+
+  //Reading the input city value
+  const handleCityChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  //fetching city weather details while submitting the form and set to the state
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=781bcd7599b51fab0d822feeb88d5ef1`
+      )
+      .then((res) => {
+        setWeather(res.data);
+        setIcon(res.data.weather[0].icon);
+        setShow(true);
+      })
+      .catch((err) => {
+        alert("Invalid City Name", err.message);
+      });
+  };
+
+  //Fetching current location weather details
+  const getCurrentLocation = async () => {
+    const { data } = await axios.get("https://ipapi.co/json");
+    const { data: weatherData } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${data?.city}&appid=781bcd7599b51fab0d822feeb88d5ef1`
+    );
+    setCurrentLocation(data.city);
+    setWeather(weatherData);
+    setShow(true);
+    setIcon(weatherData.weather[0].icon);
+  };
+
+  return (
+    <div>
+      {show ? (
+        <Weather
+          city={city}
+          setCity={setCity}
+          currentLocation={currentLocation}
+          weather={weather}
+          setShow={setShow}
+          icon={icon}
+        />
+      ) : (
+        <div className="container">
+          <header id="city">Weather App</header>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Enter city name"
+              value={city}
+              onChange={handleCityChange}
+            />
+          </form>
+          <p id="or">or</p>
+          <button onClick={getCurrentLocation}>Get Device Location</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Weather.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./Weather.css";
+import { MdLocationPin } from "react-icons/md";
+import { ImArrowLeft2 } from "react-icons/im";
+import { BsDropletHalf, BsThermometerSun } from "react-icons/bs";
+
+const Weather = (props) => {
+  const { city, setCity, setShow, weather, icon, currentLocation } = props;
+
+  // console.log(city);
+
+  //toggling the setShow value to false
+  const handleBackClick = () => {
+    setShow(false);
+    setCity("");
+  };
+
+  return (
+    <div className="container-weather">
+      <header>
+        <ImArrowLeft2 id="back-button" onClick={handleBackClick} />
+        <span id="heading">Weather App</span>
+      </header>
+      <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt="" />
+      <p id="temp">{(weather.main.temp - 270).toFixed(0)}&#176; C</p>
+      <p id="weather">{weather.weather[0].main}</p>
+      <p id="location">
+        <MdLocationPin className="location-icon" />
+        {city.length > 0 ? city : currentLocation}
+      </p>
+      <footer>
+        <div id="weather-details-container">
+          <div id="row1">
+            <BsThermometerSun id="feels-like-icon" />
+          </div>
+          <div id="row2">
+            <span id="feels-like-temp">
+              {(weather.main.feels_like - 270).toFixed(0)}&#176;C
+            </span>
+            <span id="feels-like-text">Feels like</span>
+          </div>
+          <div id="row3">
+            <BsDropletHalf id="humidity-icon" />
+          </div>
+          <div id="row4">
+            <span id="humidity-temp">{weather.main.humidity}%</span>
+            <span id="humidity-text">Humidity</span>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Weather;
